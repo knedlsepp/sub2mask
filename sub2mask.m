93629c7f1176804d8826e9c0bcae6de8d0d5a556
Josef Kemetmüller <josef.kemetmueller@aon.at>
2015-03-15 01:50:06 +0100

    More descriptions

--- sub2mask.m
+++ sub2mask.m
@@ -1,4 +1,24 @@
 function mask = sub2mask(siz,varargin)
+%SUB2MASK Combine subscripts and logical indexing to a logical mask.
+%   SUB2MASK is used to simplify the access of multidimensional arrays.
+%
+%   MASK = SUB2MASK(SIZ, varargin) returns a logical mask of size SIZ,
+%   according to the subscript or logical indices provided by varargin.
+%
+%   Example: We access all layers of a 3D array according to a diagonal
+%   mask:
+%
+%   A = rand(10,10,4);
+%   A(SUB2MASK(size(A),eye(10)~=0,:));
+%   
+%   This could be simplified via an anonymous function:
+%
+%   I = @(A,varargin) SUB2MASK(size(A),varargin{:});
+%   A(I(A,eye(10)~=0,:))
+%
+%   I = @(varargin) SUB2MASK(size(A), varargin{:});
+%   A(I(eye(10)~=0,:))
+
 mask = true;
 d = 1;
 for k = 1:numel(varargin)
